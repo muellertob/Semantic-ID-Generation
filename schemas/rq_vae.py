ab829d3d596b40d4784c4829f7820b756edc8604
Tobias <tobias.mueller99@icloud.com>
2026-03-22 21:12:01 +0100

    improved logging of RQ-VAE training

--- schemas/rq_vae.py
+++ schemas/rq_vae.py
@@ -14,3 +14,9 @@ class RqVaeComputedLosses(NamedTuple):
     rqvae_loss: Tensor
     embs_norm: Tensor
     p_unique_ids: Tensor
+    layer_coverages: Tensor       # (n_layers,) fraction of codebook used per layer
+    layer_entropies: Tensor       # (n_layers,) assignment entropy per layer
+    first_residual_norm: Tensor   # scalar: ||residual after layer 0|| / ||encoder output||
+    last_residual_norm: Tensor    # scalar: ||residual after last layer|| / ||encoder output||
+    first_centroids_norm: Tensor  # scalar: mean norm of first-layer codebook vectors
+    last_centroids_norm: Tensor   # scalar: mean norm of last-layer codebook vectors
